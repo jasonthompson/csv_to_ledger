16e79dbd78dee343fa13ac8687c63ad9a5ea3e23
Jason Thompson <jason@jthompson.ca>
2013-12-16 06:03:34 -0500

    prompt_for_account creates new STDOUT
    - In order to allow csv-to-ledger to pipe output to a file,
    prompt_for_acount has to use new instance of STDOUT.

--- lib/csv_to_ledger/cli.rb
+++ lib/csv_to_ledger/cli.rb
@@ -1,7 +1,9 @@
 module CSVToLedger
   class CLI
-    attr_reader :input_file, :csv_settings
+    attr_reader :input_file 
+    attr_reader :csv_settings
     attr_reader :vendor_account_matcher
+    attr_accessor :output
 
     def initialize(input_file)
       @input_file = input_file
@@ -9,6 +11,7 @@ module CSVToLedger
                       :col_sep => ',',
                       :header_converters => [lambda {|h| h.strip}, :symbol]}
       @vendor_account_matcher = VendorAccountMatcher.new
+      @output = ""
     end
 
     def title_caps(str)
@@ -24,32 +27,37 @@ module CSVToLedger
     end
 
     def prompt_for_account(transaction)
-      puts("No account for #{transaction.summary}.")
-      puts("Please enter an existing (tab to autocomplete) or a new one.")
+      fd = IO.sysopen("/dev/tty", "r+")
+      out = IO.new(fd, "w+")
+
+      out.puts("No account for #{transaction.summary}.")
+      out.puts("Please enter an existing (tab to autocomplete) or a new one.")
 
       Readline.completion_proc = proc do |input|
         vendor_account_matcher.accounts.grep(/^#{Regexp.escape(input)}/)
       end
 
+      Readline.output = out
       account = Readline.readline('account: ', true)
       vendor_account_matcher.add_vendor_account(transaction.description, account)
       account
     end
 
     def run
-      output = ""
-
       CSV.foreach(input_file, csv_settings) do |row|
-        output << Transaction.new do |t|
-          t.date = row[:date]
-          t.description = filter_description(row[:transaction_details])
-          t.funds_out = row[:funds_out]
-          t.funds_in = row[:funds_in]
-          t.account = vendor_account_matcher.match(t.description) || prompt_for_account(t).capitalize
-        end
+        output << create_transaction(row)
       end
+      puts output
+    end
 
-      output
+    def create_transaction(row)
+      Transaction.new do |t|
+        t.date = row[:date]
+        t.description = filter_description(row[:transaction_details])
+        t.funds_out = row[:funds_out]
+        t.funds_in = row[:funds_in]
+        t.account = vendor_account_matcher.match(t.description) || prompt_for_account(t).capitalize
+      end
     end
   end
 end
